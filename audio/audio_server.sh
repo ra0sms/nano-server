ee8a3f4f0d47e058eae15f1b47ccb2177d6ccf2e
ra0sms <ra0sms@bk.ru>
2026-08-04 21:36:55 +0800

    add cutoff audio filter for 50Hz

--- audio/audio_server.sh
+++ audio/audio_server.sh
@@ -45,7 +45,9 @@ echo "  Buffer Time: $BUFFER_TIME us"
 
 gst-launch-1.0 alsasrc device=hw:0 buffer-time=$BUFFER_TIME latency-time=1000 ! \
 audioconvert ! audioresample ! \
-audioconvert ! audiocheblimit mode=high-pass cutoff=100 ! \
+audioconvert ! audiochebband mode=band-reject lower-frequency=45 upper-frequency=55 poles=4 ! \
+audiocheblimit mode=high-pass cutoff=300 ! \
+audiocheblimit mode=high-pass cutoff=300 ! \
 audioconvert ! capsfilter caps="audio/x-raw,rate=$RATE,channels=1,format=S16LE" ! \
-opusenc bitrate=48000 bitrate-type=vbr frame-size=20 complexity=2 ! rtpopuspay ! \
+opusenc bitrate=48000 bitrate-type=vbr frame-size=20 complexity=5 ! rtpopuspay ! \
 udpsink host=$IP_ADDRESS port=5000 sync=false
