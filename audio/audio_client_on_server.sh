2ff0ade1c26cb820f2748cc1873679845adef23d
ra0sms <ra0sms@bk.ru>
2026-08-04 19:45:34 +0800

    add chebishev HPF

--- audio/audio_client_on_server.sh
+++ audio/audio_client_on_server.sh
@@ -7,5 +7,6 @@
 gst-launch-1.0 udpsrc port=5000 caps="application/x-rtp,payload=96" ! \
 rtpjitterbuffer latency=100 drop-on-latency=false ! \
 queue max-size-time=50000000 leaky=downstream ! \
-rtpopusdepay ! opusdec plc=true ! queue ! audioconvert ! queue ! \
+rtpopusdepay ! opusdec plc=true ! audiocheblimit mode=high-pass cutoff=80 ! \
+queue ! audioconvert ! queue ! \
 alsasink device=hw:0 buffer-time=100000 latency-time=1000 sync=false
